0cdf696ccffd1bae6610a7ce04ee228086542955
Wagner Macedo <wagnerluis1982@gmail.com>
2018-03-07 20:37:18 -0300

    Reading database settings from URL

--- helloreport/settings.py
+++ helloreport/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from functools import partial
+
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,11 +77,10 @@ WSGI_APPLICATION = 'helloreport.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+db_from_url = partial(dj_database_url.parse, conn_max_age=600)
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': db_from_url('sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')),
 }
 
 
