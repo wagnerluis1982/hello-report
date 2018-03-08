a702ef0b77dbb00cb8cdfcb9e94570c155e557b6
Wagner Macedo <wagnerluis1982@gmail.com>
2018-03-08 17:18:20 -0300

    Create model Invoice
    I will use that to store the invoices imported from the sales system.

--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Invoice(models.Model):
+    SALE = 'S'
+    SKIPPED = 'K'
+    CANCELED = 'C'
+    DENIED = 'D'
+    SALE_RETURN = 'U'
+    PURCHASE_RETURN = 'P'
+    RMA = 'R'
+
+    NATURE_CHOICES = (
+        (SALE, _('Sale')),
+        (SKIPPED, _('Skipped')),
+        (CANCELED, _('Canceled')),
+        (DENIED, _('Denied')),
+        (SALE_RETURN, _('Sale return')),
+        (PURCHASE_RETURN, _('Purchase return')),
+        (RMA, _('RMA')),
+    )
+
+    number = models.PositiveIntegerField(_('number'), primary_key=True)
+    date = models.DateField(_('date'))
+    customer = models.CharField(_('customer'), max_length=60, null=True, blank=True)
+    nature = models.CharField(_('nature'), max_length=1, choices=NATURE_CHOICES)
+    total = models.FloatField(_('total'), null=True, blank=True)
+    comment = models.CharField(_('comment'), max_length=100, null=True, blank=True)
+
+    class Meta:
+        verbose_name = _('invoice')
+        verbose_name_plural = _('invoices')
+
+    def __str__(self):
+        return "%06d - %s" % (self.number, self.customer)
